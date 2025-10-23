1e5bd2ccb8df8879d98500f0d19308478ebf3dda
dingzhenyao <2363441530@qq.com>
2025-10-23 10:32:44 +0800

    增加了一个模拟后端，其中带有两个角色数据，将登陆部分的axios改成了api，api添加了一个baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,14 +2,19 @@ import axios from "axios"
 import { useAuthStore } from "../stores/auth"
 
 const api = axios.create({
+  baseURL: 'http://localhost:3001',
   timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
 
-const authStore = useAuthStore()
+//const authStore = useAuthStore()
 
 // 请求拦截器
 api.interceptors.request.use(
   (config) => {
+    const authStore = useAuthStore()
     if (authStore.token) {
       config.headers.Authorization = `Bearer ${authStore.token}`
     }
